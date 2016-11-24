264c82d34d529e5810db24fda3de3f299f583d58
Radek <Radek@HPDESK>
2016-11-23 18:06:50 -0800

    Add project files.

---
+++ TripLog/TripLog/Models/TripLogEntry.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace TripLog
+{
+	public class TripLogEntry
+	{
+		public TripLogEntry()
+		{
+			
+		}
+
+		public string Title
+		{
+			get;
+			set;
+		}
+
+		public double Latitute
+		{
+			get;
+			set;
+		}
+
+		public double Longitude
+		{
+			get;
+			set;
+		}
+
+		public DateTime Date
+		{
+			get;
+			set;
+		}
+
+		public int Rating
+		{
+			get;
+			set;
+		}
+
+		public string Notes
+		{
+			get;
+			set;
+		}
+	}
+}
---
+++ TripLog/TripLog/TripLogPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace TripLog
+{
+    public partial class TripLogPage : ContentPage
+    {
+        public TripLogPage()
+        {
+            Title = "TripLog";
+
+            var items = new List<TripLogEntry>
+            {
+                new TripLogEntry
+                {
+                    Title = "Washington Monument",
+                    Notes = "Amazing!",
+                    Rating = 3,
+                    Date = new DateTime(2016,01,03),
+                    Latitute = 38.8895,
+                    Longitude = -770352
+                },
+                new TripLogEntry
+                {
+                    Title = "Statue of Liberty",
+                    Notes = "Inspiring",
+                    Rating = 4,
+                    Date = new DateTime(2016,03,13),
+                    Latitute = 40.6892,
+                    Longitude = -74.0444
+                },
+                new TripLogEntry
+                {
+                    Title = "Golden Gate Bridge",
+                    Notes = "Foggy but beautiful",
+                    Rating = 5,
+                    Date = new DateTime(2016,04,12),
+                    Latitute = 37.8268,
+                    Longitude = -122.4798
+                }
+            };
+
+            var itemTemplate = new DataTemplate(typeof(TextCell));
+            itemTemplate.SetBinding(TextCell.TextProperty, "Title");
+            itemTemplate.SetBinding(TextCell.DetailProperty, "Notes");
+
+            var listViewEntries = new ListView
+            {
+                ItemsSource = items,
+                ItemTemplate = itemTemplate
+            };
+
+            Content = listViewEntries;
+        }
+    }
+}
